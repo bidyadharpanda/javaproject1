2151f1807b62c0c79b09ebc8cc5a4eafd72cf670
bidyadharpanda <46751166+bidyadharpanda@users.noreply.github.com>
2019-01-17 21:34:06 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,20 @@
+package SampleTest;
+
+public class Test {
+	int a;
+	int b;
+	Test(int x, int y){
+		this.a=x;
+		this.b=y;
+		
+	}
+	public void add() {
+		System.out.println("Addition of two numbers is:"+(a+b));
+		
+	}
+	public void multiply() {
+		System.out.println("Multiplication of two numbers is:"+(a*b));
+		
+	}
+
+}
---
+++ Testproject.java
@@ -0,0 +1,16 @@
+package SampleTest;
+
+public class Testproject {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test t=new Test(3,4);
+		t.add();
+		t.multiply();
+		
+		
+
+	}
+
+}
